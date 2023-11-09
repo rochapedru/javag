ffc75effc4891cbb733d226230dd8835b3a4e686
Joao Pedro Oliveira Rocha <pedrorocha2info@gmail.com>
2023-11-09 13:31:38 -0300

    4.3

--- aula_02/src/aula_02/Exr01.java
+++ aula_02/src/aula_02/Exr01.java
@@ -6,8 +6,8 @@ public class Exr01 {
 	static Scanner Leia = new Scanner(System.in);
 	public static void main(String[] args) {
 	int salario, abono, novoSal;
-	salario = 10000;
-	abono = 1000;
+	salario = Leia.nextInt();
+	abono = Leia.nextInt();
 	novoSal = (abono + salario);
 	System.out.print("Novo Salario "+ novoSal);
 	}
--- aula_02/src/aula_02/Exr02.java
+++ aula_02/src/aula_02/Exr02.java
@@ -1,9 +1,23 @@
 package aula_02;
 
-public class Exr02 {
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import java.util.Scanner;
 
+public class Exr02 {
+	static Scanner Leia = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		int n1, n2, n3, n4, media;
+		
+		n1 = Leia.nextInt();
+		n2 = Leia.nextInt();
+		n3 = Leia.nextInt();
+		n4 = Leia.nextInt();
+		
+		media = (n1 + n2 + n3 + n4)/4;
+		
+		System.out.println("Sua media é: " + media);	
+		
+		// TODO Auto-generated method stub
 	}
 
 }
