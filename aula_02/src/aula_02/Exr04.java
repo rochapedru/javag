761dc51044ffe330b098ea1303285a9273bdcf25
Joao Pedro Oliveira Rocha <pedrorocha2info@gmail.com>
2023-11-09 13:43:29 -0300

    4.5

--- aula_02/src/aula_02/Exr04.java
+++ aula_02/src/aula_02/Exr04.java
@@ -1,9 +1,20 @@
 package aula_02;
 
+import java.util.Scanner;
+
 public class Exr04 {
+	static Scanner Leia = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		double n1, n2, n3, n4, dif;
+
+		n1 = Leia.nextDouble();
+		n2 = Leia.nextDouble();
+		n3 = Leia.nextDouble();
+		n4 = Leia.nextDouble();
+		
+		dif = (n1 * n2) - (n3 * n4);
+		System.out.println("Diferença é " + dif);
 
 	}
 
