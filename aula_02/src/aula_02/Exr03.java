1b2e0eb0876dac00b5d58f3438719a9cfc6ce8bc
Joao Pedro Oliveira Rocha <pedrorocha2info@gmail.com>
2023-11-09 13:39:20 -0300

    4.4

--- aula_02/src/aula_02/Exr03.java
+++ aula_02/src/aula_02/Exr03.java
@@ -1,8 +1,19 @@
 package aula_02;
 
-public class Exr03 {
+import java.util.Scanner;
 
+public class Exr03 {
+	static Scanner Leia = new Scanner(System.in);
 	public static void main(String[] args) {
+		int bruto, noturno, extra, desconto, liquido;
+		
+		bruto = Leia.nextInt();
+		noturno = Leia.nextInt();
+		extra = Leia.nextInt();
+		desconto = Leia.nextInt();
+		
+		liquido = bruto + noturno + (extra * 5) - desconto;
+		System.out.println("Salario Liquido " + liquido);
 		// TODO Auto-generated method stub
 
 	}
