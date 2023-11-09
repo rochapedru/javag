d5da5b26a313424aff9732b0490a53ceab6d1644
Joao Pedro Oliveira Rocha <pedrorocha2info@gmail.com>
2023-11-09 14:29:45 -0300

    5.5

--- aula_03/src/aula_03/lista01_exr1.java
+++ aula_03/src/aula_03/lista01_exr1.java
@@ -5,7 +5,18 @@ import java.util.Scanner;
 public class lista01_exr1 {
 	static Scanner Leia = new Scanner(System.in);
 	public static void main(String[] args) {
+		int a, b, c, soma;
+		a = Leia.nextInt();
+		b = Leia.nextInt();
+		c = Leia.nextInt();
+		soma = a + b;
 		
-		System.out.print("oi");
+		if (soma > c) {
+			System.out.println("A soma de A + B: " + soma + "´é maior que C: " + c);
+		}else if (soma < c) {
+			System.out.println("A soma de A + B: " + soma + "´é menor que C: " + c);
+		}else if (soma == c) {
+			System.out.println("A soma de A + B: " + soma + "´é igual a C: " + c);
+		}
 	}
 }
