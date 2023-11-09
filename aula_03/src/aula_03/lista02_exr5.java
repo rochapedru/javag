61a38777d35f97a99e4b39e1c51d2e725946d8c2
Joao Pedro Oliveira Rocha <pedrorocha2info@gmail.com>
2023-11-09 14:44:12 -0300

    5.6

--- aula_03/src/aula_03/lista02_exr5.java
+++ aula_03/src/aula_03/lista02_exr5.java
@@ -4,8 +4,38 @@ import java.util.Scanner;
 
 public class lista02_exr5 {
 	static Scanner Leia = new Scanner(System.in);
+
 	public static void main(String[] args) {
-		
-		System.out.print("oi");
+		int cod, quant;
+		double total;
+		cod = Leia.nextInt();
+		quant = Leia.nextInt();
+
+		switch (cod) {
+		case 1:
+			total = quant * 10.00;
+			System.out.printf("Dogs por: " + total);
+			break;
+		case 2:
+			total = quant * 15.00;
+			System.out.printf("Salada por: " + total);
+			break;
+		case 3:
+			total = quant * 18.00;
+			System.out.printf("Bacon por: " + total);
+			break;
+		case 4:
+			total = quant * 12.00;
+			System.out.printf("Bauru por: " + total);
+			break;
+		case 5:
+			total = quant * 8.00;
+			System.out.printf("Refri por: " + total);
+			break;
+		case 6:
+			total = quant * 13.00;
+			System.out.printf("Suco por: " + total);
+			break;
+		}	
 	}
 }
