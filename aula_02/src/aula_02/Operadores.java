8227d6a534104a949daa2116c882139e4df8e49e
Joao Pedro Oliveira Rocha <pedrorocha2info@gmail.com>
2023-11-09 10:09:38 -0300

    2

---
+++ aula_02/src/aula_02/Operadores.java
@@ -0,0 +1,15 @@
+package aula_02;
+
+public class Operadores {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int numero = 10, numero2 = 20;
+		String valor1 = "2", valor2 = "3";
+		
+		System.out.println("soma: " + (numero + numero2));
+		
+		System.out.println("soma: " + (valor1 + valor2));
+	}
+
+}
