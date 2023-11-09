3c684e3c07ee796e4e2d4e9749271f96fb30341d
Joao Pedro Oliveira Rocha <pedrorocha2info@gmail.com>
2023-11-09 14:20:52 -0300

    5.2

--- aula_03/src/aula_03/lista01_exr02.java
+++ aula_03/src/aula_03/lista01_exr02.java
@@ -5,7 +5,16 @@ import java.util.Scanner;
 public class lista01_exr02 {
 	static Scanner Leia = new Scanner(System.in);
 	public static void main(String[] args) {
+		int num;
+		num = Leia.nextInt();
+		if (num == 0) {
+			System.out.println(num + " é zero");
+		}else if (num > 0) {
+			System.out.println(num + " é positivo");
+		}else if (num < 0) {
+			System.out.println(num + " é negativo");
+		}
 		
-		System.out.print("oi");
+
 	}
 }
