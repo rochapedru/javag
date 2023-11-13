136447127b8787277a8b170db03f46a303bac0e6
Joao Pedro Oliveira Rochaq <pedrorocha2info@gmail.com>
2023-11-13 11:12:48 -0300

    5.9.6

--- aula_05/src/aula_05/Matrizes.java
+++ aula_05/src/aula_05/Matrizes.java
@@ -4,10 +4,13 @@ import java.util.Scanner;
 
 public class Matrizes {
 	static Scanner Leia = new Scanner(System.in);
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[][] matriz = {{1,2,3}, {4,5,6}, {7,8,9}};
 		double[][] matrizDouble = new double [3][3];
+		
+		double[][] matrizIrregular = {{1,2,3}, {4,5,6}};
 	
 		for(int indiceI = 0; indiceI < matrizDouble.length; indiceI ++) {
 			for(int indiceJ = 0; indiceJ < matrizDouble.length; indiceJ ++) {
@@ -20,9 +23,26 @@ public class Matrizes {
 				System.out.println("Matriz[" + indiceI + "][" + indiceJ + "] = " + matriz[indiceI][indiceJ]);
 				
 			}
+		
+		}
+		System.out.println("\n");
+		
+		for(int indiceI = 0; indiceI < matrizIrregular.length; indiceI ++) {
+			for(int indiceJ = 0; indiceJ < matrizIrregular[indiceI].length; indiceJ ++) {
+				/* pq indice[i] sao as linhas de baixo e cima, com 123,456, duas linhas, com indice linha pega os indices j,
+				coluna */
+				System.out.println("Matriz[" + indiceI + "][" + indiceJ + "] = " + matrizIrregular[indiceI][indiceJ]);
+			}
+		}
+		System.out.println("\n");
+		
+		for(double[] vetor : matrizIrregular) {
+			for(double elemento : vetor) {
+				System.out.println(elemento);
+			}
+
 		}
 
-	}
-
+}
 
 }
