1d72de8518d6e5a1d57c4e823b2c5b8db395ba40
Joao Pedro Oliveira Rochaq <pedrorocha2info@gmail.com>
2023-11-15 19:01:10 -0300

    6.5

--- aula-06/src/exr01_list.java
+++ aula-06/src/exr01_list.java
@@ -1,9 +1,17 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class exr01_list {
-
+	static Scanner Leia = new Scanner(System.in);
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		ArrayList<String> cores = new ArrayList<String>();
+		String cor;
+		for(int i = 0; i <= 4; i ++) {
+			System.out.println("Digite uma cor: ");
+			cor = Leia.nextLine();
+			cores.add(cor);
+			
+		}
+	System.out.println(cores);
 	}
-
 }
