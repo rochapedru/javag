08d4c1861c09dd79334fe01ba24b9ac16357fc32
Joao Pedro Oliveira Rochaq <pedrorocha2info@gmail.com>
2023-11-16 00:57:50 -0300

    6.6

--- aula-06/src/exr03_set.java
+++ aula-06/src/exr03_set.java
@@ -1,4 +1,27 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Scanner;
 
 public class exr03_set {
-
+	static Scanner Leia = new Scanner(System.in);
+	public static void main(String[] args) {
+		HashSet<Integer> valores = new HashSet<Integer>();
+		
+		int num;
+		
+		for(int i = 0; i <= 9; i ++) {
+			System.out.println("Digite um numero: ");
+			num = Leia.nextInt();
+			valores.add(num);
+		
+		}
+		ArrayList<Integer> numList = new ArrayList<Integer>();
+		Iterator<Integer> z = valores.iterator();
+		while(z.hasNext()) {
+			numList.add(z.next());
+		}
+		numList.sort(null);
+		System.out.println(numList);
+	}
 }
--- aula-06/src/exr04_set.java
+++ aula-06/src/exr04_set.java
@@ -1,4 +1,7 @@
 
 public class exr04_set {
-
+	public static void main(String[] args) {
+		
+		
+	}
 }
